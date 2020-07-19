da31df846fb84e3f21b9c91ba302ff0159cd704c
Valério Cardoso <valeriocardoso@outlook.com.br>
2020-07-19 18:36:56 -0300

    studied up to Path Parameters

--- API/FastApi/main.py
+++ API/FastApi/main.py
@@ -1,14 +1,23 @@
-from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Hello World"}
+
 
 @app.get("/item/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: int):
+    return {"item_id": item_id}
 
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
