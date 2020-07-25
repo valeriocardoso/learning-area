f17d84be294758efd613f7d677678861e6f4ddda
Valério Cardoso <valeriocardoso@outlook.com.br>
2020-07-24 21:12:19 -0300

    Preparaing the environment

---
+++ Programing/GO/fundamentals/01/01.go
@@ -0,0 +1,16 @@
+// Programas executáveis iniciam pelo pacote main
+package main
+
+import "fmt"
+
+/*
+Os códigos em Go são organizados em pacotes
+e para usá-los é necessários declarar um ou vários imports
+*/
+
+func main() {
+
+	fmt.Print("Primeiro")
+	fmt.Print("Programa!")
+
+}
